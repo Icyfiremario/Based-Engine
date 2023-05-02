7de205e8bd23af73a347315687e63d097912820f
Icyfiremario <icyfiremario797@gmail.com>
2023-05-02 10:59:51 -0600

    Added protection to MIxerManager.hpp

--- MixerManager.hpp
+++ MixerManager.hpp
@@ -1,3 +1,6 @@
+#ifndef MixerManager_hpp
+#define MixerManager_hpp
+
 #include "Game.hpp"
 #include "AssetManager.hpp"
 #include <SDL2/SDL_mixer.h>
@@ -18,3 +21,5 @@ class MixerManager
         Mix_Chunk* sfx;
         Mix_Music* music;
 };
+
+#endif /* MixerManager_hpp */
