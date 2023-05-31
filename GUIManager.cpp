19becbc396585ece9a0107107a83f72b368a5da8
Icyfiremario <icyfiremario797@gmail.com>
2023-05-31 00:21:45 -0600

    renamed GUI to GUIManager and added basic stuff

---
+++ GUIManager.cpp
@@ -0,0 +1,31 @@
+#include "GUIManager.hpp"
+
+GUIManager::GUIManager(Manager* man)
+{}
+
+GUIManager::~GUIManager()
+{}
+
+void GUIManager::newGUI(std::string id, bool isMenu)
+{
+    GUIs.emplace(id, isMenu);
+}
+
+void GUIManager::newGUI(std::string id, bool isMenu, int sizeX, int sizeY)
+{
+    GUIs.emplace(id, isMenu, sizeX, sizeY);
+}
+
+GUI* GUIManager::toggleMenu(std::string id)
+{
+    GUI* gui = GUIs[id];
+
+    if(!gui->isMenu)
+    {
+        PLOGI << "Menu Toggled";
+    }
+    else
+    {
+        PLOGW << "Attempted to toggle non menu GUI";
+    }
+}
---
+++ GUIManager.hpp
@@ -0,0 +1,47 @@
+#ifndef GUIManager_HPP
+#define GUIManager_HPP
+
+#include "SDL2/SDL.h"
+#include <string>
+#include <map>
+#include "ECS/ECS.hpp"
+#include "Game.hpp"
+
+class GUIManager
+{
+    public:
+
+        GUIManager(Manager* man);
+        ~GUIManager();
+
+        void newGUI(std::string id, bool isMenu);
+        void newGUI(std::string id, bool isMenu, int sizeX, int sizeY);
+
+        GUI* toggleMenu(std::string id);
+
+    private:
+
+        Manager* manager;
+
+        std::map<std::string, GUI*> GUIs;
+
+
+        
+
+};
+
+class GUI
+{
+    public:
+
+        std::string id;
+        bool isMenu;
+
+        int sizeX;
+        int sizeY;
+
+        
+};
+
+
+#endif /* GUIManager_HPP */
